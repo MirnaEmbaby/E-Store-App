979b72502e32560c9654e06feb573d45e27f0e97
mirnaembaby <mirnaembaby@gmail.com>
2024-05-29 20:59:56 +0300

    settings problem

--- lib/modules/shop_register_screen/shop_register_screen.dart
+++ lib/modules/shop_register_screen/shop_register_screen.dart
@@ -1,13 +1,177 @@
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
+import 'package:e_store/modules/shop_layout/layout_screen.dart';
+import 'package:e_store/modules/shop_register_screen/cubit/cubit.dart';
+import 'package:e_store/modules/shop_register_screen/cubit/states.dart';
+import 'package:e_store/shared/components/components.dart';
+import 'package:e_store/shared/components/constants.dart';
+import 'package:e_store/shared/network/local/cache_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+// ignore: must_be_immutable
 class ShopRegisterScreen extends StatelessWidget {
-  const ShopRegisterScreen({super.key});
+  var formKey = GlobalKey<FormState>();
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+  var nameController = TextEditingController();
+  var phoneController = TextEditingController();
+
+  ShopRegisterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: const Column(),
+    return BlocProvider(
+      create: (context) => ShopRegisterCubit(),
+      child: BlocConsumer<ShopRegisterCubit, ShopRegisterStates>(
+        listener: (context, state) {
+          if (state is ShopRegisterSuccessState) {
+            if (state.loginModel.status!) {
+              debugPrint(state.loginModel.message);
+              debugPrint(state.loginModel.data?.token);
+
+              CacheHelper.saveData(
+                      key: 'token', value: state.loginModel.data?.token)
+                  .then((value) {
+                token = state.loginModel.data!.token!;
+                navigateAndFinish(context, const LayoutScreen());
+              });
+            } else {
+              debugPrint(state.loginModel.message);
+
+              showToast(
+                state.loginModel.message,
+                ToastStates.error,
+              );
+            }
+          }
+        },
+        builder: (context, state) {
+          return Scaffold(
+            appBar: AppBar(),
+            body: Center(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'REGISTER',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineMedium!
+                              .copyWith(
+                                color: Colors.black,
+                              ),
+                        ),
+                        Text(
+                          'Register now to browse our hot offers',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyLarge!
+                              .copyWith(color: Colors.grey),
+                        ),
+                        const SizedBox(
+                          height: 30.0,
+                        ),
+                        defaultFormField(
+                          controller: nameController,
+                          type: TextInputType.name,
+                          validate: (String value) {
+                            if (value.isEmpty) {
+                              return 'Please, enter your username';
+                            }
+                          },
+                          label: 'Username',
+                          prefix: Icons.person,
+                          hasSuffix: false,
+                        )!,
+                        const SizedBox(
+                          height: 15.0,
+                        ),
+                        defaultFormField(
+                          controller: emailController,
+                          type: TextInputType.emailAddress,
+                          validate: (String value) {
+                            if (value.isEmpty) {
+                              return 'Please, enter your email address';
+                            }
+                          },
+                          label: 'Email address',
+                          prefix: Icons.email_outlined,
+                          hasSuffix: false,
+                        )!,
+                        const SizedBox(
+                          height: 15.0,
+                        ),
+                        defaultFormField(
+                          controller: passwordController,
+                          type: TextInputType.visiblePassword,
+                          validate: (String value) {
+                            if (value.isEmpty) {
+                              return 'Password is too short';
+                            }
+                          },
+                          label: 'Password',
+                          prefix: Icons.lock_outline,
+                          hasSuffix: true,
+                          isPassword:
+                              ShopRegisterCubit.get(context).isPasswordShown,
+                          suffix: ShopRegisterCubit.get(context).suffix,
+                          suffixPressed: () {
+                            ShopRegisterCubit.get(context).changeVisibility();
+                          },
+                        )!,
+                        const SizedBox(
+                          height: 15.0,
+                        ),
+                        defaultFormField(
+                          controller: phoneController,
+                          type: TextInputType.phone,
+                          validate: (String value) {
+                            if (value.isEmpty) {
+                              return 'Please, enter your phone number';
+                            }
+                          },
+                          label: 'Phone',
+                          prefix: Icons.phone,
+                          hasSuffix: false,
+                        )!,
+                        const SizedBox(
+                          height: 30.0,
+                        ),
+                        ConditionalBuilder(
+                          condition: state is! ShopRegisterLoadingState,
+                          builder: (context) => defaultButton(
+                            function: () {
+                              if (formKey.currentState!.validate()) {
+                                ShopRegisterCubit.get(context).userRegister(
+                                  email: emailController.text,
+                                  password: passwordController.text,
+                                  name: nameController.text,
+                                  phone: phoneController.text,
+                                );
+                              }
+                              FocusScope.of(context).unfocus();
+                            },
+                            text: 'Register',
+                            isUpperCase: true,
+                          ),
+                          fallback: (context) => const Center(
+                            child: CircularProgressIndicator(),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
